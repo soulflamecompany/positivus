171569a6667f9a949c801695c271f26104222341
Filipp Kadushkin <kadushkin.feniks@gmail.com>
2024-11-16 22:29:04 +0300

    feat: implement header/burger/styles and successfully set webpack build-dev up

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
 import "./index.html";
 import "./index.scss";
-import code from "./img/code.png";
-import { mult, sum } from "./modules/helpers";
-
-const imgWrapper = document.querySelector(".img");
-const img = new Image();
-img.src = code;
-img.width = 800;
-imgWrapper.append(img);
-
-console.log(mult(66, 66));
-console.log(sum(66, 66));
